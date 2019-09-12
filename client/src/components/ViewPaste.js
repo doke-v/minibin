0938aa0aa23d956fcd7397ae1d0cf21c209dfdbc
Janar Liiver <janar.liiver@netgroup.ee>
2019-09-13 00:13:07 +0300

    wip

--- client/src/components/ViewPaste.js
+++ client/src/components/ViewPaste.js
@@ -5,6 +5,9 @@ import style from "react-syntax-highlighter/dist/esm/styles/hljs/gruvbox-dark";
 function ViewPaste(props) {
   const [data, setData] = useState("");
   const id = props.match.params.id
+  let hash = props.location.hash
+  hash = hash.substring(1);
+
   const getData = id => {
     fetch("/bin/api/" + id + "?_=" + new Date().valueOf())
       .then(response => {
@@ -18,8 +21,13 @@ function ViewPaste(props) {
     getData(id);
   }, [id]);
 
+  if(hash === "raw") {
+    return <pre className="raw"><code>{data}</code></pre>
+  }
+
   return (
     <SyntaxHighlighter
+      language={hash}
       showLineNumbers
       lineNumberStyle={{ opacity: "0.3", userSelect: "none" }}
       customStyle={{ height: "100%", margin: 0 }}
