7bd11cd4b48f3a14da8954c73a4a35e219d23148
tuhnik <huntlarsen@gmail.com>
2019-09-14 05:06:30 +0300

    wip

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 require('dotenv').config()
-const url = process.env.MONGODB_URL;
+const MONGODB_URL = process.env.MONGODB_URL;
 const express = require('express')
 const app = express()
 
 const pasteRoutes = require('./api/routes/pastes')
 const mongoose = require('mongoose')
 
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 
 app.use(express.json())
 app.use('/bin/api', pasteRoutes)
